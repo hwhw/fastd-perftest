da7d78f4070266b2bf06e0f4f761848b4e19e631
Hans-Werner Hilse <hwhilse@gmail.com>
2015-03-25 00:11:42 +0100

    make full test suite

--- run-fastd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-fastd -c 1/fastd.conf &
-F1=$!
-fastd -c 2/fastd.conf &
-F2=$!
-echo press enter to kill fastd instances
-read x
-kill $F1
-kill $F2
---
+++ run-test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+fastd -c 1/fastd.conf &
+F1=$!
+fastd -c 2/fastd.conf &
+F2=$!
+echo waiting a bit
+sleep 2
+iperf -V -s &
+IPF1=$!
+sleep 1
+F1IP=`ip -6 addr show fast1|awk '/ +inet6/{print $2}'|awk 'BEGIN{FS="/"}{print $1}'`
+iperf -V -c $F1IP%fast2
+kill $F1
+kill $F2
+kill $IPF1
